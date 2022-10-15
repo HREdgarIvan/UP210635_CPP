ff7e5ad3417d96d545c69b5dd6e201d3442a104b
UP210635 <UP210635@alumnos.upa.edu.mx>
2022-10-15 09:41:38 -0500

    si

---
+++ U2/E10_Switchvase.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int v1=6,v2=2;
+    int opcion=4;
+    double=r;
+      cout << "operacion <1..4>";
+    cin >> opcion;
+    while (opcion !=0)
+    {
+    
+    switch (opcion)
+    {
+    case 1:
+          cout << "suma"<< endl;
+        break;
+    case 2:
+          cout << "resta"<< endl; 
+     break;
+    case 3:
+          cout << "multi"<< endl; 
+     break;
+    case 4:
+          cout << "division"<< endl;
+
+     break;  
+    default:
+         cout << "opcion invalida"<< endl; 
+        break;
+    }
+    cout << "operacion <1..4>";
+    cin >> opcion;
+    }
+    return 0;
+}
